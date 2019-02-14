34c7cb0b1ddc02830cf7fea5968919630fa48dc0
Lady Pulido <ladypulido94@gmail.com>
2019-02-14 16:27:22 -0700

    The api call works, the carousel of images need to be fixed, and we need to add the characteristics of the cats

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
-document.getElementById("catSubmit").addEventListener("click", function (event) {
-    event.preventDefault();
-    const value = document.getElementById("catInput").value;
-    if (value === "")
-        return;
-    console.log(value);
+$(document).ready(function () {
 
-    //var data = null;
+    document.getElementById("catSubmit").addEventListener("click", function (event) {
+        event.preventDefault();
+        const value = document.getElementById("catInput").value;
+        if (value === "")
+            return;
+        console.log(value);
 
-    //var xhr = new XMLHttpRequest();
-    //xhr.withCredentials = true;
-
-    //xhr.addEventListener("readystatechange", function () {
-    //    if (this.readyState === this.DONE) {
-    //        console.log(this.responseText);
-    //    }
-    //});
-
-    //xhr.open("GET", "https://api.thecatapi.com/v1/breeds?attach_breed=" + value);
-    //xhr.setRequestHeader("x-api-key", "6314d1a9-cbbd-4597-986d-934c4e60c6a6");
-    //xhr.send(data);
-
-    var settings = {
-        "async": true,
-        "crossdomain": true,
-        "url": "https://api.thecatapi.com/v1/breeds?attach_breed=" + value,
-        "method": "get",
-        "headers": {
-            "x-api-key": "6314d1a9-cbbd-4597-986d-934c4e60c6a6"
+        var catImg = {
+            "async": true,
+            "crossdomain": true,
+            "url": "https://api.thecatapi.com/v1/images/search?breed_id=" + value + "&limit=8",
+            "method": "get",
+            "headers": {
+                "x-api-key": "6314d1a9-cbbd-4597-986d-934c4e60c6a6"
+            }
         }
-    }
-    console.log(settings);
 
-    $.ajax(settings).done(function (response) {
-        console.log(response);
+        $.ajax(catImg).done(function (response) {
+            console.log(response);
+            for (let i = 0; i < response.length; i++) {
+                let picture = response[i]["url"];
+                console.log(picture);
+
+                $(`<div class="carousel-item"><img id = "catPic" src="${picture}" width="50%"> </div>`).appendTo('.carousel-inner');
+                $('<li data-target="#carousel" data-slide-to="' + i + '"></li>').appendTo('.carousel-indicators');
+            }
+
+            $('.carousel-item').first().addClass('active');
+            $('.carousel-indicators > li').first().addClass('active');
+
+        });
+
     });
 });
